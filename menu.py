07164c7d36942f603994f159b34f994fceda13ad
FareehaYakkub <139794351+FareehaYakkub@users.noreply.github.com>
2023-08-21 22:47:42 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -31,5 +31,5 @@ class Menu:
         for item in self.menu:
             if item.name == order_name:
                 return item
-            print("Sorry that item is not available.")
-            return 'nil'
+        print("Sorry that item is not available.")
+        return 'nil'
